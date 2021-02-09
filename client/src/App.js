dd0ff5125b828a0cb1795e1706ef73cb2eb3fcd7
miadehaan <64979457+miadehaan@users.noreply.github.com>
2021-02-08 21:48:26 -0800

    new compent added - Person

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from "./components/Person";
 
 class App extends Component {
-  render() {
+  
+  render () {
     return (
       <div className="App">
         <h1> Udemy Project! </h1>
+        <Person name="Max" age="25" />
+        <Person name="Mia" age="50"> I love to eat ice cream </Person>
       </div>
-    );
+    )
   }
+
 }
 
 export default App;
